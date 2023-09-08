414c95670fc1297347e4aa8fcf3df5ee815ae283
lixinjack <lixinjack@gmail.com>
2023-09-08 13:11:10 -0700

    09/08/2023 upadte

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FollowPlayer::Start()
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+// 0x00000002 System.Void FollowPlayer::LateUpdate()
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+// 0x00000003 System.Void FollowPlayer::.ctor()
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+// 0x00000004 System.Void PlayerControl::Start()
+extern void PlayerControl_Start_mFADBD79243A6B6984704BCCACC7320F96F4D34C7 (void);
+// 0x00000005 System.Void PlayerControl::Update()
+extern void PlayerControl_Update_m3A67C6EE4E8A895323A173E7D58C82ECC002C428 (void);
+// 0x00000006 System.Void PlayerControl::.ctor()
+extern void PlayerControl__ctor_m2B3A599A61BE9CCA98820EB2390E965CBE920915 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	PlayerControl_Start_mFADBD79243A6B6984704BCCACC7320F96F4D34C7,
+	PlayerControl_Update_m3A67C6EE4E8A895323A173E7D58C82ECC002C428,
+	PlayerControl__ctor_m2B3A599A61BE9CCA98820EB2390E965CBE920915,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	984,
+	984,
+	984,
+	984,
+	984,
+	984,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Library/Bee/artifacts/emscripten/q8rj0/wrapper_emcc.bat
@@ -0,0 +1,4 @@
+
+@echo off
+"D:\editor\2021.3.30f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
+exit %ERRORLEVEL%
